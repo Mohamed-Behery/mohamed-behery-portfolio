bd721373f5e5de1d425801771511f041af9f0163
Mohamed Behery <mohamedbehery.main@gmail.com>
2023-06-14 23:10:35 +0300

    Added a Filter To The Projects Section

--- scripts/main.js
+++ scripts/main.js
@@ -40,3 +40,32 @@ menuToggle.addEventListener("click", function () {
     menuToggle.innerHTML = '<i class="fas fa-bars"></i>';
   }
 });
+
+// Projects Filter
+
+let projectsList = document.querySelectorAll(".projects-list");
+let projectCard = document.querySelectorAll(".project-card");
+
+for (let i = 0; i < projectsList.length; i++) {
+  projectsList[i].addEventListener("click", function () {
+    for (let j = 0; j < projectsList.length; j++) {
+      projectsList[j].classList.remove("active");
+    }
+
+    this.classList.add("active");
+
+    let projectsFilter = this.getAttribute("data-filter");
+
+    for (let k = 0; k < projectCard.length; k++) {
+      projectCard[k].classList.add("hide");
+      projectCard[k].classList.remove("active");
+      if (
+        projectCard[k].getAttribute("data-item") == projectsFilter ||
+        projectsFilter == "all"
+      ) {
+        projectCard[k].classList.add("active");
+        projectCard[k].classList.remove("hide");
+      }
+    }
+  });
+}
