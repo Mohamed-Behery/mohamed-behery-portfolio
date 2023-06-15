457cffbb80128f02627f852ba784ebe3c531acc7
Mohamed Behery <mohamedbehery.main@gmail.com>
2023-06-15 23:47:33 +0300

    Added a Details Overlay in Projects Section

--- scripts/main.js
+++ scripts/main.js
@@ -69,18 +69,3 @@ for (let i = 0; i < projectsList.length; i++) {
     }
   });
 }
-
-// Project Popup Page
-
-const projectPage = document.querySelector(".project-page");
-const popupOpenBtn = document.querySelector("#popup-open-btn");
-const popupCloseBtn = document.querySelector(".popup-close-btn");
-
-popupOpenBtn.addEventListener("click", function () {
-  projectPage.classList.add("active");
-});
-
-popupCloseBtn.addEventListener("click", function () {
-  projectPage.classList.remove("active");
-  projectPage.classList.remove("active");
-});
